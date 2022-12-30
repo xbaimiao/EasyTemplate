1d852b49214f4c9e58afd902f05aabf844b90d2b
小白 <3104026189@qq.com>
2022-12-30 15:44:36 +0800

    移动包到kotlin

---
+++ src/main/kotlin/com/xbaimiao/template/EasyTemplate.kt
@@ -0,0 +1,16 @@
+package com.xbaimiao.template
+
+import com.xbaimiao.easylib.EasyPlugin
+
+@Suppress("unused")
+class EasyTemplate : EasyPlugin() {
+
+    override fun enable() {
+
+    }
+
+    override fun disable() {
+
+    }
+
+}
