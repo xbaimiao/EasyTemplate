12b712f6eda62914424c785c88c0b063f8717186
xbaimiao <3104026189@qq.com>
2024-04-14 22:02:05 +0800

    使用仓库管理插件

--- buildSrc/src/main/kotlin/com/xbaimiao/easylib/Relocate.kt
+++
@@ -1,7 +0,0 @@
-package com.xbaimiao.easylib
-
-data class Relocate(val pattern: String, val replacement: String, val cloud: Boolean) {
-
-    constructor(pattern: String, replacement: String) : this(pattern, replacement, true)
-
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.xbaimiao.com/repository/maven-public/")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
 rootProject.name = "EasyTemplate"
