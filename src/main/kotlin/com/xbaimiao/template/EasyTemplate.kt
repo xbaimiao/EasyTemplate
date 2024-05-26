ceea203ff59e2454f238688954b68cf6b8c9371c
xbaimiao <3104026189@qq.com>
2024-05-26 14:04:07 +0800

    test

--- src/main/kotlin/com/xbaimiao/template/EasyTemplate.kt
+++ src/main/kotlin/com/xbaimiao/template/EasyTemplate.kt
@@ -2,11 +2,13 @@ package com.xbaimiao.template
 
 import com.xbaimiao.easylib.EasyPlugin
 import com.xbaimiao.easylib.util.ShortUUID
+import com.xbaimiao.easylib.util.info
 
 @Suppress("unused")
 class EasyTemplate : EasyPlugin() {
 
     override fun enable() {
+        info("测试")
         logger.info("${description.name} 插件启动成功 ${ShortUUID.randomShortUUID()}")
     }
 
